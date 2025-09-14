b41566946c54294f341e46f7ce9a785ac0bbb097
Niyaz94 <niyaz94nz@gmail.com>
2025-09-14 16:34:34 +0200

    Finishing Alert componet for Expense limit.

---
+++ src/components/Custom/Alert.tsx
@@ -0,0 +1,40 @@
+
+
+import {FC,useState,useEffect} from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+
+interface AlertProps {
+    title:string;
+    message:string;
+    severity: 'error' | 'warning' | 'info' | 'success';
+    openDefault: boolean;
+}
+const CustomAlert:FC<AlertProps> = ({title,message,severity='success',openDefault=false}) => {
+
+
+    console.log("Alert message",message)
+    const [open, setOpen] = useState(openDefault);
+
+    useEffect(() => {
+        setOpen(openDefault);
+    }, [openDefault,message]);
+
+
+
+    return <Collapse in={open} >
+      <Alert severity={severity} action={
+        <IconButton aria-label="close" color="inherit" size="medium" onClick={() => {setOpen(false);}}>
+          <CloseIcon fontSize="inherit" />
+        </IconButton>
+      }>
+        <AlertTitle>{title}</AlertTitle>
+        {message}
+      </Alert>
+    </Collapse>
+}
+
+export default CustomAlert;
--- src/content/applications/Expense/AddEdit.tsx
+++ src/content/applications/Expense/AddEdit.tsx
@@ -1,10 +1,8 @@
 import { usePostAPI, useEditAPI, useFetch, FetchData }         from "../../../utility/customHook";
 
-import React, { useState,useEffect,useCallback } from 'react';
+import React, { useState,useEffect,useCallback, use } from 'react';
 import {Card,CardHeader,CardContent,Divider,Box,TextField} from '@mui/material';
-// import Grid from '@mui/material/Grid2';
 import Grid from '@mui/material/Grid';
-
 
 import { useNavigate,useParams }    from 'react-router-dom';
 import LexicalEditor                from '../../../components/Custom/Lexical/Editor';
@@ -14,7 +12,9 @@ import {ExpenseFormIntialState as FormIntialState}  from "../../../utility/funct
 
 import { useSelector }              from 'react-redux';
 import StaticAutocomplete           from '../../../components/Form/StaticAutocomplete';
+import CustomAlert                  from '../../../components/Custom/Alert';
 
+import {axiosGetData} from '../../../utility/Axios'
 
 import { RootState }                    from '../../../store/Reducer';
 import { ExpenseFormIntialStateInterface as FormIntialStateInterface } from '../../../utility/types/Page';
@@ -40,10 +40,14 @@ const CollapsibleForm = () => {
   const [formData, setFormData] = useState(FormIntialState);
   const dailyData               = useSelector((state: RootState) => state.daily.data);
 
+  const [alertData, setAlertData] = useState<{title:string,message:string,severity:'error' | 'warning' | 'info' | 'success',openDefault:boolean}>({title:"",message:"",severity:"success",openDefault:false});
+
 
   const { data:fetchEditData,success:editReturnSuccess}: FetchData<FormIntialStateInterface>  = useFetch <FormIntialStateInterface>(edit_page_id ?`money/expense/${edit_page_id}`: null,{});
   const { loading:post_api_loading, error:post_api_error, success,response, postData}   = usePostAPI();
   const { response:editResponse, loading:editLoading, error:editError, editData}        = useEditAPI();
+
+
 
   const saveReturn=()=>{
     handleSave().then(()=>navigate('/transactions/expense'))
@@ -60,7 +64,6 @@ const CollapsibleForm = () => {
 
   useEffect(() => {
       const {success,data}=editResponse || {success:false,data:null};
-      console.log("editResponse",data)
       setTable(prev => success ?[...prev.map((item:SingleSampleInterface) => item.id === data.id?data:item),data]:prev);
   }, [editResponse]);
 
@@ -89,7 +92,27 @@ const CollapsibleForm = () => {
     setFormData(FormIntialState)
   };
 
-  return (
+  const checkRemainingAmount = () => {
+    
+    if (formData.category>0 && formData.currency>0)
+      axiosGetData(`money/expense/total_expense/?categoryId=${formData.category}&currencyId=${formData.currency}&date=${formData.date}`).then((res) => {
+        const {limit,expense,remaining,month} = res.data;
+        if(limit==null || limit==0){
+            setAlertData({...alertData,openDefault:false})
+        }else if (Number(remaining) <= 0){
+          setAlertData({title:"Limit Exceeded",message:`You have exceeded your limit for this category. You have used ${expense} out of ${limit} ${currencyTypeMap.filter((item) => item.value === formData.currency)[0]?.label || ""} for ${month}.`,severity:"error",openDefault:true})
+        }else if (Number(remaining) <= (0.1 * Number(limit))){
+          setAlertData({title:"Warning",message:`You are close to your limit for this category. You have used ${expense} out of ${limit} ${currencyTypeMap.filter((item) => item.value === formData.currency)[0]?.label || ""} for ${month}.`,severity:"warning",openDefault:true})
+        }else{
+          setAlertData({title:"Info",message:`You have used ${expense} out of ${limit} ${currencyTypeMap.filter((item) => item.value === formData.currency)[0]?.label || ""} for ${month}.`,severity:"info",openDefault:true})
+        }
+      });
+  }
+  useEffect(() => {
+    checkRemainingAmount();
+  }, [formData.category,formData.date,formData.currency]);
+
+  return ( 
       <Card>
         <CardHeader title="Input Fields" />
         <Divider />
@@ -99,13 +122,20 @@ const CollapsibleForm = () => {
           >
             <Grid container spacing={2}>
 
+              <Grid size={12}>
+                {useCallback(() => <CustomAlert {...alertData} />, [alertData])()}
+              </Grid>
+
+
               <Grid size={6}>
                 <StaticAutocomplete
                   label="Select Expense Type"
                   options={expenseTypeMap}
                   defaultValue={expenseTypeMap.filter(({label,value}) => value == Number(formData.category))[0]}
                   formKey="category"
-                  onChange={handleFormChange}
+                  onChange={(key, value) => {
+                    handleFormChange(key, value);   
+                  }}
                 />
               </Grid>
 
